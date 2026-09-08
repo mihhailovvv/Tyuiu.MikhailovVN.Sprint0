6941a7199485fa6f2ef64c2a60bdf7576f94baec
Валерий Михайлов < @DESKTOP-JG59SMF>
2026-09-08 23:47:15 +0500

    Add project files.

---
+++ Tyuiu.MikhailovVN.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.MikhailovVN.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
