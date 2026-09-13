5533e2fd0c6e103d8ef1f7009e439f2dffc48c23
Валерий Михайлов <Tyuiu.MikhailovVN@outlook.com>
2026-09-13 22:09:57 +0500

    Task4

---
+++ Tyuiu.MikhailovVN.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿
+using Tyuiu.MikhailovVN.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.MikhailovVN.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Additional(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckedMultiolicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
